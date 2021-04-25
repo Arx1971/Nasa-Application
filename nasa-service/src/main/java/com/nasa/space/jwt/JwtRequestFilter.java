07a37a197fa543c4ff389a3db788c806fd7ebc53
Adnan Rahin <adnan.rahin13@gmail.com>
2021-04-25 05:15:40 -0400

    JwtAuthenticationEntryPoint implemented

---
+++ nasa-service/src/main/java/com/nasa/space/jwt/JwtRequestFilter.java
@@ -0,0 +1,4 @@
+package com.nasa.space.jwt;
+
+public class JwtRequestFilter {
+}
---
+++ nasa-service/src/main/java/com/nasa/space/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nasa.space.repositories;
+
+import com.nasa.space.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
