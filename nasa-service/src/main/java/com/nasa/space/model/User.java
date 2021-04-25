b3c209c973db69da4d30aba09f67f8af9849ba44
Adnan Rahin <adnan.rahin13@gmail.com>
2021-04-25 04:22:35 -0400

    User DAO Added

---
+++ nasa-service/src/main/java/com/nasa/space/model/User.java
@@ -0,0 +1,43 @@
+package com.nasa.space.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "nasa_space_exploration_database")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "users_id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
