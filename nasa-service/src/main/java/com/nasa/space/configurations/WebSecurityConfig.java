f0e44389296681afb7dc9f118caec9b6cf5b398e
Adnan Rahin <adnan.rahin13@gmail.com>
2021-04-25 06:12:48 -0400

    update

--- nasa-service/src/main/java/com/nasa/space/configurations/WebSecurityConfig.java
+++ nasa-service/src/main/java/com/nasa/space/configurations/WebSecurityConfig.java
@@ -1,12 +1,22 @@
 package com.nasa.space.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
