18f5251bf43b13d3eecc672736e24bd55b960398
Adnan Rahin <adnan.rahin13@gmail.com>
2021-05-23 19:32:34 -0400

    University Repostiry Created

---
+++ nasa-service/src/main/java/com/nasa/space/repositories/UniversityRepository.java
@@ -0,0 +1,9 @@
+package com.nasa.space.repositories;
+
+import com.nasa.space.model.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UniversityRepository extends JpaRepository<University, Long> {
+}
