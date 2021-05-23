f087da3a91581e5619c54e5d469196f7bb8a3038
Adnan Rahin <adnan.rahin13@gmail.com>
2021-05-23 18:59:49 -0400

    Hashcode Generated

--- nasa-service/src/main/java/com/nasa/space/model/University.java
+++ nasa-service/src/main/java/com/nasa/space/model/University.java
@@ -2,6 +2,7 @@ package com.nasa.space.model;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "university_info")
@@ -15,4 +16,32 @@ public class University {
     @Column
     private String universityName;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof University)) return false;
+        University that = (University) o;
+        return Objects.equals(id, that.id) && Objects.equals(universityName, that.universityName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, universityName);
+    }
 }
