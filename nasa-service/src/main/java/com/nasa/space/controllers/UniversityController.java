5d683737675f57e467d650eff99970777a4b469d
Adnan Rahin <adnan.rahin13@gmail.com>
2021-05-24 23:07:11 -0400

    Application Controller

---
+++ nasa-service/src/main/java/com/nasa/space/controllers/UniversityController.java
@@ -0,0 +1,14 @@
+package com.nasa.space.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/api/university")
+public class UniversityController {
+
+
+
+}
