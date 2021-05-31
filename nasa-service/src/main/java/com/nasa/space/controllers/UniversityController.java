c6e8e9a9a5e0f78236b0800c20862dcd952e33c5
Adnan Rahin <adnan.rahin13@gmail.com>
2021-05-31 03:03:29 -0400

    University Controller Added

--- nasa-service/src/main/java/com/nasa/space/controllers/UniversityController.java
+++ nasa-service/src/main/java/com/nasa/space/controllers/UniversityController.java
@@ -1,14 +1,32 @@
 package com.nasa.space.controllers;
 
+import com.nasa.space.model.University;
+import com.nasa.space.services.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/university")
 public class UniversityController {
 
+    private UniversityService universityService;
 
+    @Autowired
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getAllUniversity")
+    public ResponseEntity<List<University>> getUniversity() {
+        List<University> universities = universityService.findAll();
+        return ResponseEntity.ok(universities);
+    }
 
 }
