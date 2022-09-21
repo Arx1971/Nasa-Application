9a1c58ffd76617dd3a93d19ea85608b1295ebeb5
adnanrahin <adnan.rahin13@gmail.com>
2022-09-21 00:29:12 -0400

    Unit test added

---
+++ nasa-service/src/test/java/com/nasa/space/controllers/AstronautControllerTest.java
@@ -0,0 +1,25 @@
+package com.nasa.space.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AstronautControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAstronautsInfo() {
+    }
+
+    @Test
+    void getAstronautById() {
+    }
+
+    @Test
+    void saveAstronaut() {
+    }
+}
